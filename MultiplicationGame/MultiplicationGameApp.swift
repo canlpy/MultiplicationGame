2cd73d2205bbe292099d723b5edb7700746c2ca4
CNLPY <calpay@yahoo.com>
2022-06-22 12:44:46 +0300

    Initial Commit

---
+++ MultiplicationGame/MultiplicationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationGameApp.swift
+//  MultiplicationGame
+//
+//  Created by Can on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
